6e9a4ab7bbf49703de90f22df7f02526f1689af1
Microbit2 <95872969+Microbit2@users.noreply.github.com>
2021-12-09 17:39:39 +0100

    Temperaturanzeige Bonus 2

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+// Temperaturanzeige Bonus 2
+basic.forever(function () {
+    if (input.temperature() > 25) {
+        basic.showIcon(IconNames.Happy)
+    } else if (input.temperature() < 15) {
+        basic.showIcon(IconNames.Sad)
+    } else if (input.temperature() == Math.constrain(input.temperature(), 15, 25)) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    }
+})
